9df50abee8cd8ebe6f8f99b6950543129cdcbe63
Creativeguru97 <worldapple2630@gmail.com>
2020-08-15 02:32:41 +0900

    August 14, 2020

--- Co-workAgent/sketch.js
+++ Co-workAgent/sketch.js
@@ -5,17 +5,44 @@ let background0;
 let background1;
 let flares = [];
 
+//Agent activity
+let agentApperance = [];
+let typing_L0 = [];
+let typing_L1 = [];
+let typing_R0 = [];
+let thinking = [];
+let index = 0;
+let frameIncliment = 0;
+
 canvas = p => {
+
+  p.preload = () => {
+    background0 = p.loadImage("data/background/background0.png");
+    background1 = p.loadImage("data/background/background1.png");
+    for(let i=0; i<3; i++){
+      flares[i] = p.loadImage("data/optical_effect/flare" + i + ".png");
+    }
+
+    for(let i=0; i<5; i++){
+      typing_L0[i] = p.loadImage("data/agent_typing/L0/" + p.nf(i, 2) + ".png");
+    }
+    for(let i=0; i<15; i++){
+      typing_L1[i] = p.loadImage("data/agent_typing/L1/" + p.nf(i, 2) + ".png");
+    }
+    for(let i=0; i<5; i++){
+      typing_R0[i] = p.loadImage("data/agent_typing/R0/" + p.nf(i, 2) + ".png");
+    }
+
+    for(let i=0; i<9; i++){
+      thinking[i] = p.loadImage("data/agent_thinking/" + p.nf(i, 2) + ".png");
+    }
+
+  }
 
 
   p.setup = () => {
     p.createCanvas(960, 540);
-    background0 = p.loadImage("animations/background/background0.png");
-    background1 = p.loadImage("animations/background/background1.png");
-    for(let i=0; i<3; i++){
-      flares[i] = p.loadImage("animations/optical_effect/flare" + i + ".png");
-    }
-
+    p.frameRate(30);
     p.imageMode(p.CENTER);
   }
 
@@ -26,13 +53,23 @@ canvas = p => {
     p.image(background1, 480, 270, 960, 540);
     p.image(background0, 480, 270, 960, 540);
 
+    //This is where the agent comes in.
+    index += frameIncliment;
+    if(index > typing_L0.length - 2){
+      frameIncliment = -1;
+    }else if (index < 1) {
+      frameIncliment = 1;
+    }
+    console.log(index);
+    p.image(typing_L0[index], 480, 270, 960, 540);
+
     //Add some optical flare effects!
     p.blendMode(p.SCREEN);
     p.image(flares[0], 1118, 102, 1600 * 1.5, 889 * 1.5);
     p.image(flares[1], 497, 285, 1600 * 0.2, 1333 * 0.2);
     p.image(flares[2], 932, 280, 1600 * 0.2, 1333 * 0.2);
 
-    
+
 
   }
 
