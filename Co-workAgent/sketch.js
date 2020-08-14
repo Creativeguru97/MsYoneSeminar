0f6eb8f9eb4e193486f9c09444053e3682821075
Creativeguru97 <worldapple2630@gmail.com>
2020-08-15 01:01:08 +0900

    August 15, 2020

--- Co-workAgent/sketch.js
+++ Co-workAgent/sketch.js
@@ -1,16 +1,39 @@
 
 
 let canvas;
+let background0;
+let background1;
+let flares = [];
 
 canvas = p => {
 
 
   p.setup = () => {
     p.createCanvas(960, 540);
+    background0 = p.loadImage("animations/background/background0.png");
+    background1 = p.loadImage("animations/background/background1.png");
+    for(let i=0; i<3; i++){
+      flares[i] = p.loadImage("animations/optical_effect/flare" + i + ".png");
+    }
+
+    p.imageMode(p.CENTER);
   }
 
   p.draw = () => {
-    p.background(100);
+
+    //Display background
+    p.blendMode(p.BLEND);
+    p.image(background1, 480, 270, 960, 540);
+    p.image(background0, 480, 270, 960, 540);
+
+    //Add some optical flare effects!
+    p.blendMode(p.SCREEN);
+    p.image(flares[0], 1118, 102, 1600 * 1.5, 889 * 1.5);
+    p.image(flares[1], 497, 285, 1600 * 0.2, 1333 * 0.2);
+    p.image(flares[2], 932, 280, 1600 * 0.2, 1333 * 0.2);
+
+    
+
   }
 
 
